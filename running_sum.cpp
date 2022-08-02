806afea2334eff4ac497f3bb0e4ff7abd7b321c3
Jrana1 <jewelbutex12@gmail.com>
2022-08-02 16:36:48 +0200

    running array sum function has been added

--- running_sum.cpp
+++ running_sum.cpp
@@ -1,11 +1,25 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> runningSum(vector<int> &nums)
+{
+    // cout << "fjla" << endl;
+    vector<int> ans;
+    ans.push_back(nums[0]);
+    for (int i = 1; i < nums.size(); i++)
+    {
+        ans.push_back(nums[i] + ans[i - 1]);
+    }
+    return ans;
+}
 
 int main()
 {
-
-    cout << "fjajflajf" << endl;
-
+    vector<int> nums = {1, 1, 1, 1, 1};
+    nums = runningSum(nums);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << endl;
+    }
     return 0;
 }
