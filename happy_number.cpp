c926dcc7d610d32a42b2187e706b026e9f3653ef
Jrana1 <jewelbutex12@gmail.com>
2022-08-18 23:51:28 +0200

    function to check if a number is happy or not has been solved

---
+++ happy_number.cpp
@@ -0,0 +1,48 @@
+// problem link: https://leetcode.com/problems/happy-number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isHapp(int n)
+{
+    set<string> st;
+
+    while (1)
+    {
+
+        string num = to_string(n);
+        if (st.count(num) > 0)
+        {
+            return false;
+        }
+        st.insert(num);
+        int square_sum = 0;
+        for (int i = 0; i < num.size(); i++)
+        {
+            int x = num[i] - '0';
+            square_sum += x * x;
+        }
+        cout << square_sum << endl;
+        if (square_sum == 1)
+        {
+            return true;
+        }
+        else
+        {
+            n = square_sum;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    cout << isHapp(n) << endl;
+
+    return 0;
+}
