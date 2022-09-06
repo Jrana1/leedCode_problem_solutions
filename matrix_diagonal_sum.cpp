4fe6888ff89c6d77a20e7466d82f92bc78587261
Jrana1 <jewelbutex12@gmail.com>
2022-09-06 21:25:58 +0200

    matrix giagonal some problem has benn soleved

---
+++ matrix_diagonal_sum.cpp
@@ -0,0 +1,28 @@
+// problem link: https://leetcode.com/problems/matrix-diagonal-sum/
+
+#include <bits/stdc++.h>
+using namespace std;
+int diagonalSum(vector<vector<int>> &mat)
+{
+
+    int sum1 = 0;
+    int sum2 = 0;
+
+    for (int i = 0; i < mat.size(); i++)
+    {
+        sum1 += mat[i][i];
+        int n = mat.size() - 1 - i;
+        if (i != n)
+        {
+            sum2 += mat[i][n];
+        }
+    }
+    return sum1 + sum2;
+}
+int main()
+{
+
+    vector<vector<int>> mat = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    diagonalSum(mat);
+    return 0;
+}
