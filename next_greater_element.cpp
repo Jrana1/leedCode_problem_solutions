08755a4f7256604f9dcc5d351ac9cfa03296cb3b
Jrana1 <jewelbutex12@gmail.com>
2022-09-09 18:55:00 +0200

    function to find next greater element has been added

--- next_greater_element.cpp
+++ next_greater_element.cpp
@@ -5,39 +5,57 @@ using namespace std;
 
 vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
 {
-    vector<int> res;
-    for (int i = 0; i < nums1.size(); i++)
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums2.size(); i++)
     {
 
-        for (int j = 0; j < nums2.size(); j++)
-        {
+        mp.insert(pair<int, int>(nums2[i], i));
+    }
+    vector<int> res;
+    bool found_next_greater = false;
+    for (auto x = 0; x < nums1.size(); x++)
+    {
 
-            if (nums1[i] == nums2[j] && j == nums2.size() - 1)
+        auto it = mp.find(nums1[x]);
+        if (it != mp.end())
+        {
+            if (it->second == nums2.size() - 1)
             {
                 res.push_back(-1);
+                continue;
             }
-            else if (nums1[i] == nums2[j] && nums2[j + 1] > nums2[j])
+            int j;
+            for (j = it->second + 1; j < nums2.size(); j++)
             {
-                res.push_back(nums2[j + 1]);
+                if (nums2[j] > it->first)
+                {
+                    res.push_back(nums2[j]);
+                    break;
+                }
             }
-            else if (nums1[i] == nums2[j] && nums2[j + 1] < nums2[j])
+            if (j == nums2.size())
             {
+
                 res.push_back(-1);
             }
         }
     }
-    return res;
-}
-
-int main()
-{
-    vector<int> nums1 = {1, 3, 5, 2, 4};
-    vector<int> nums2 = {6, 5, 4, 3, 2, 1, 7};
-    vector<int> res = nextGreaterElement(nums1, nums2);
     for (auto x : res)
     {
         cout << x << " ";
     }
+}
+
+int main()
+{
+    vector<int> nums1 = {2, 1, 3};
+    vector<int> nums2 = {2, 3, 1};
+    vector<int> res = nextGreaterElement(nums1, nums2);
+    // for (auto x : res)
+    // {
+    //     cout << x << " ";
+    // }
 
     return 0;
 }
