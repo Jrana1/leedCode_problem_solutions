7f7a826d1fe7c508fb41b598851a6f8c96bb2941
Jrana1 <jewelbutex12@gmail.com>
2022-09-16 23:40:43 +0200

    function to find longest substring without repeating char has been added

---
+++ longest_substring_without_repeating_char.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Naive solution O(N^3) time and O(N) space complexity
+int lengthOfLongestSubstring(string s)
+{
+    int n = s.size();
+    int len = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            set<char> st;
+            string str;
+            for (int k = i; k <= j; k++)
+            {
+                st.insert(s[k]);
+                str += s[k];
+                // cout << s[k];
+            }
+            // cout << endl;
+            if (st.size() == str.size())
+            {
+                int r = st.size();
+                len = max(len, r);
+            }
+        }
+    }
+    // cout << len << endl;
+}
+int lengthOfLongestSubstring1(string s)
+{
+
+    int max_len = 0;
+    int l = 0, r = 0;
+    unordered_set<char> st;
+    while (s[r] != '\0')
+    {
+
+        if (st.find(s[r]) == st.end())
+        {
+            st.insert(s[r]);
+            r++;
+        }
+        else
+        {
+            while (st.find(s[r]) != st.end())
+            {
+
+                st.erase(s[l]);
+                l++;
+            }
+            st.insert(s[r]);
+            r++;
+        }
+        max_len = max(max_len, r - l);
+    }
+    return max_len;
+}
+int main()
+{
+
+    // string s = "abcabcbb";
+    // // cout << s.size() << endl;
+    // cout << lengthOfLongestSubstring1(s) << endl;
+    vector<int> v1(10);
+    vector<int> v2(10);
+    v1[2] = 2;
+    v2[2] = 2;
+    cout << (v1 == v2) << endl;
+    return 0;
+}
